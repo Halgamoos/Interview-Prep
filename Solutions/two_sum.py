0f3e7d8064dd53d6e33c052936ba4e97b30274ce
Halgamoos <halgamoos@gmail.com>
2021-08-20 04:26:49 -0400

    two sum

---
+++ Solutions/two_sum.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/two-sum/
+
+# brute force
+# O(n^2) time | O(1) space
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+# Two-pass Hash Table
+# O(n) time | O(n) space
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+
+# One-pass Hash Table
+# O(n) time | O(n) space
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
