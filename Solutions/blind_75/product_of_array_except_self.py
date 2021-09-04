c2347eae5790f8bdd266ca9c101ad1b5b4185939
Halgamoos <halgamoos@gmail.com>
2021-09-04 01:38:18 -0400

    updated product of array except self

---
+++ Solutions/blind_75/product_of_array_except_self.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/product-of-array-except-self/
+
+# Shrinking Window (Two Pass)
+# O(n) time | O(1) time
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        
+        prefix = 1
+        for i in range(len(nums)):
+            result[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            result[i] *= postfix
+            postfix *= nums[i]
+        return result
