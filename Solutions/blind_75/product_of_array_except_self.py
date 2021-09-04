f9c749d91c92a9b0c22742f2b53924988f7e9ba9
Halgamoos <halgamoos@gmail.com>
2021-09-04 02:45:23 -0400

    added test to question

--- Solutions/blind_75/product_of_array_except_self.py
+++ Solutions/blind_75/product_of_array_except_self.py
@@ -3,16 +3,26 @@
 # Two Pass
 # O(n) time | O(1) time
 
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
-        
-        prefix = 1
-        for i in range(len(nums)):
-            result[i] = prefix
-            prefix *= nums[i]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            result[i] *= postfix
-            postfix *= nums[i]
-        return result
+
+def productExceptSelf(nums):
+    result = [1] * len(nums)
+    
+    prefix = 1
+    for i in range(len(nums)):
+        result[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        result[i] *= postfix
+        postfix *= nums[i]
+    return result
+
+test = [
+    [[1, 2 , 3 , 4], [24, 12, 8, 6]],
+    [[1, 2 , 3 , 4], [24, 12, 8, 6]],
+    [[1, 2 , 3 , 4], [24, 12, 8, 6]]
+]
+
+for input_array, correct_result in test:
+    my_result = productExceptSelf(input_array)
+    print(input_array, correct_result, my_result, my_result == correct_result, sep = " | ")
